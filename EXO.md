beb9fe1fcd341a52c885abf37436871389c91748
Jensone <hello@jensone.com>
2024-12-26 09:58:57 +0100

    Ajout d'un nouveau fichier Exo.md et amélioration de plusieurs exercices dans 1-10.js pour la prise de commande et les paiements

---
+++ EXO.md
@@ -0,0 +1,12 @@
+# Borne de commandes resto
+
+---
+
+Le restaurant **Prepa Cuizine** recherche un système qui permettrait de faciliter la prise de commande et les paiements de celles-ci en plus de employé en caisse. Vous avaez entendu parleer de borne de commande connectées au web et qui permettent d'utiliser une application web dédiée à la gestion des commandes et des paiements.
+
+## Exo
+
+Choisissez une de solution apporté par la borne de commandes et rédiger un algorithme permettant de réaliser la fonctionnalité demandée :
+
+- Gestion des commandes
+- Gestion des paiements
--- js/1-10.js
+++ js/1-10.js
@@ -75,3 +75,115 @@ if (prix > 200) {
 } else {
     console.log(prix * 1.2);
 }
+
+// ### Exercice 11 : Déterminer la Catégorie d'un Enfant en Fonction de son Âge
+
+// #### Algorithme :
+
+// ```
+// 1. Entrer l'âge de l'enfant (Age)
+let age = prompt("Entrer l'âge de l'enfant");
+
+switch (age) {
+    case age <= 5:
+        console.log("Pas de catégorie");
+        break;
+    case age < 6:
+        console.log("Enfant de 6 à 7 ans");
+        break;
+    case age < 8:
+        console.log("Écolier de 8 à 9 ans");
+        break;
+    case age < 10:
+        console.log("Enfant de 10 à 11 ans");
+        break;
+    default:
+        console.log("Cadet après 12 ans");
+}
+// 2. Si l'âge est entre 6 et 7 ans :
+//     3. Afficher "Enfant de 6 à 7 ans"
+// 4. Sinon, si l'âge est entre 8 et 9 ans :
+//     5. Afficher "Écolier de 8 à 9 ans"
+// 6. Sinon, si l'âge est entre 10 et 11 ans :
+//     7. Afficher "Enfant de 10 à 11 ans"
+// 8. Sinon :
+//     9. Afficher "Cadet après 12 ans"
+// ```
+
+// ### Exercice 12 : Afficher les Mois en Lettres Selon le Numéro Entré
+
+// #### Algorithme :
+
+// ```
+// 1. Entrer un numéro de mois (Mois)
+// 2. Si l'utilisateur entre 1 :
+//     3. Afficher "Janvier"
+// 4. Sinon, si l'utilisateur entre 2 :
+//     5. Afficher "Février"
+// 6. Continuer de la même manière pour les autres mois...
+// ```
+
+const mois = {
+    1: "Janvier",
+    2: "Février",
+    3: "Mars",
+    4: "Avril",
+    5: "Mai",
+    6: "Juin",
+    7: "Juillet",
+    8: "Août",
+    9: "Septembre",
+    10: "Octobre",
+    11: "Novembre",
+    12: "Décembre"
+}
+
+let selection = prompt("Entrer un numéro de mois");
+
+console.log(months[selection]);
+
+
+
+// ### Exercice 13 : Afficher "Bonsoir" 10 Fois en Utilisant la Boucle While
+
+// #### Algorithme :
+
+// ```
+// 1. Initialiser un compteur (i) à 1
+let i = 1;
+
+// 2. Tant que i est inférieur ou égal à 10 :
+while (i <= 10) {
+    //     3. Afficher "Bonsoir"
+    console.log("Bonsoir");
+    //     4. Incrémenter le compteur i
+    i++;
+}
+// ```
+
+// ### Exercice 14 : Calculer la Somme de 1 à 10 en Utilisant la Boucle While
+
+// #### Algorithme :
+
+// ```
+// 1. Initialiser une variable de somme (Somme) à 0
+// 2. Initialiser un compteur (i) à 1
+// 3. Tant que i est inférieur ou égal à 10 :
+//     4. Ajouter i à la somme (Somme)
+//     5. Incrémenter le compteur i
+// 6. Afficher la somme (Somme)
+// ```
+
+// ### Exercice 15 : Calculer la Somme de 1 à N en Utilisant la Boucle While
+
+// #### Algorithme :
+
+// ```
+// 1. Entrer un nombre N
+// 2. Initialiser une variable de somme (Somme) à 0
+// 3. Initialiser un compteur (i) à 1
+// 4. Tant que i est inférieur ou égal à N :
+//     5. Ajouter i à la somme (Somme)
+//     6. Incrémenter le compteur i
+// 7. Afficher la somme (Somme)
+// ```
